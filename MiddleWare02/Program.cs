9af7775fb12af0468b097eef9bc0d4c0907faa8c
MRLIGHT02 <saifali42020@gmail.com>
2025-05-15 15:06:26 +0530

    #

--- MiddleWare02/Program.cs
+++ MiddleWare02/Program.cs
@@ -5,9 +5,6 @@ app.Run(async (HttpContext context) =>
 {
     await context.Response.WriteAsync("hello");
 });
-app.Run(async (HttpContext context) =>
-{
-    await context.Response.WriteAsync("hello again");
-});
+
 
 app.Run();
