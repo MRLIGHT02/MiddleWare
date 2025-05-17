251719952235eeba751743cfb6064659f968f039
MRLIGHT02 <saifali42020@gmail.com>
2025-05-17 17:16:15 +0530

    #

--- MiddleWare02/CustomMiddleWare/CustomMiddleWareClass.cs
+++ MiddleWare02/CustomMiddleWare/CustomMiddleWareClass.cs
@@ -1,9 +1,13 @@
 ﻿
 
+
 namespace MiddleWare02.CustomMiddleWare
 {
     public class CustomMiddlWare : IMiddleware
     {
-
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            await context.Response.WriteAsync("Middle Ware Piple Is Started");
+        }
     }
 }
